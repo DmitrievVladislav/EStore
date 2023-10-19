d8ab2b8432e57e596ccc3c17f918a44c6f828271
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-19 12:12:41 +0300

    XML Parser updated and modified

--- offers/views.py
+++ offers/views.py
@@ -13,40 +13,50 @@ class XMLParser():
 
     def get_data_from_xml(self):
         tree = ET.parse('C:\\Users\\Inform\\PycharmProjects\\coreapp\\test.xml')
-        root = tree.getroot()
-        return root
+        return tree.getroot()
 
-    def get_category_model(self):
-        tree = ET.parse('C:\\Users\\Inform\\PycharmProjects\\coreapp\\test.xml')
-        root = tree.getroot()
-        print(root.tag)
-        for elem in root:
-            category_title = elem.text
-            category_id = elem.get('id')
-            category_parent_id = elem.get('parentId', None)
-            category_picture = elem.get('picture', None)
-            category_background_color = elem.get('backgroundColor', None)
-            category_text_color = elem.get('textColor', None)
-            category = Category.objects.filter(id=category_id).first()
-            parent_category = Category.objects.filter(id=category_parent_id).first()
+    def get_category_dicts_list(self):
+        root = self.get_data_from_xml()
+        categories = []
+        for category_elem in root.findall('.//category'):
+            category = dict()
+            category['id'] = category_elem.get('id')
+            category['title'] = category_elem.text
+            category['parent_id'] = category_elem.get('parentId', None)
+            category['picture'] = category_elem.get('picture', None)
+            category['background_color'] = category_elem.get('backgroundColor', None)
+            category['text_color'] = category_elem.get('textColor', None)
+            categories.append(category)
+        return categories
+    def create_category(self, parent_category, category_dict):
+        category = Category(
+            title=category_dict['title'],
+            id=category_dict['id'],
+            parent=parent_category,
+            picture=category_dict['picture'],
+            background_color=category_dict['background_color'],
+            text_color=category_dict['text_color']
+        )
+        category.save()
+
+    def update_category(self, category, parent_category, category_dict):
+        category.title = category_dict['title']
+        category.id = category_dict['id']
+        category.parent = parent_category
+        category.picture = category_dict['picture']
+        category.background_color = category_dict['background_color']
+        category.text_color = category_dict['text_color']
+        category.save()
+
+    def add_xml_categories_in_db(self):
+        categories = self.get_category_dicts_list()
+        for category_dict in categories:
+            category = Category.objects.filter(id=category_dict['id']).first()
+            parent_category = Category.objects.filter(id=category_dict['parent_id']).first()
             if category:
-                category.title = category_title
-                category.id = category_id
-                category.parent = parent_category
-                category.picture = category_picture
-                category.background_color = category_background_color
-                category.text_color = category_text_color
-                category.save()
+                self.update_category(category, parent_category, category_dict)
             else:
-                category = Category(
-                    title=category_title,
-                    id=category_id,
-                    parent=parent_category,
-                    picture=category_picture,
-                    background_color=category_background_color,
-                    text_color=category_text_color
-                )
-                category.save()
+                self.create_category(parent_category, category_dict)
 
 
 class OfferView(APIView):
@@ -59,15 +69,8 @@ class OfferView(APIView):
             500: "Серверная ошибка"},
     )
     def get(self, request):
+        parent_category = Category.objects.filter(id=None).first()
         xmlp = XMLParser()
-        xmlp.get_data_from_xml()
-        xmlp.get_category_model()
+        xmlp.add_xml_categories_in_db()
         return Response(status=status.HTTP_200_OK)
 
-# Create your views here.
-# print('Title:', elem.text)
-# print('ID:', elem.get('id', None))
-# print('parentId:', elem.get('parentId', None))
-# print('picture:', elem.get('picture', None))
-# print('backgroundColor:', elem.get('backgroundColor', None))
-# print('textColor:', elem.get('textColor', None))
