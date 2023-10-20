1a69a874fb8b723faed191c0a25c96be36295db0
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-20 17:36:46 +0300

    settings updated

--- coreapp/settings.py
+++ coreapp/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'carts',
     'users',
     'orders',
-    'offerss',
+    'offers',
     'xml_parser'
 ]
 
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -147,3 +147,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
