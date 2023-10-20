0a3e5a42a234b53b333d0d16761d15f90a9690f7
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-20 10:44:54 +0300

    field changed from string to char

--- orders/models.py
+++ orders/models.py
@@ -3,16 +3,16 @@ from django.db import models
 from products.models import Product
 from users.models import User
 
+
 class Order(models.Model):
     products = models.ManyToManyField(Product, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     address = models.TextField(default='Воронеж', null=True)
     total_sum = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     total_quantity = models.IntegerField(default=0)
-    status = models.TextField(max_length=256, default='В обработке')
+    status = models.CharField(max_length=255, default='В обработке')
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'orders'
         ordering = ('-created',)
-
