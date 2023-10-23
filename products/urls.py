e429656bcea8c1be9162f58a1819e9f1b740e0c7
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 13:14:41 +0300

    refactored

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductsView.as_view()),
-    path('products/<int:id>/', views.SingleProductView.as_view()),
+    path('products/<int:product_id>/', views.SingleProductView.as_view()),
     path('products/recommends/', views.RecommendedProductsView.as_view()),
 ]
