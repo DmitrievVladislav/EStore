a6cfdbe1dc35a215b8bdbf414941163a42eb91b5
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 05:55:43 +0300

    refactored

--- categories/views.py
+++ categories/views.py
@@ -1,9 +1,10 @@
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from products.models import Product
 
+from products.models import Product
 from products.serializers import ProductsSerializer
 from .models import Category
 from .serializers import CategorySerializer
@@ -20,6 +21,8 @@ class CategoriesView(APIView):
     )
     def get(self, request):
         categories = Category.objects.all()
+        if not categories:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
@@ -34,7 +37,9 @@ class ProductsCategoryView(APIView):
             500: "Серверная ошибка"}
     )
     def get(self, request, category_id):
-        category_products = Product.objects.filter(categories=category_id)
+        category_products = Product.objects.filter(categories=category_id).prefetch_related('categories')
+        if not category_products:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serialized_categories = ProductsSerializer(category_products, many=True)
         return Response(serialized_categories.data)
 
@@ -50,48 +55,7 @@ class SingleCategoryDetails(APIView):
     )
     def get(self, request, category_id):
         categories = Category.objects.filter(id=category_id).first().get_descendants()
+        if not categories:
+            return Response(status=status.HTTP_404_NOT_FOUND)
         serialized_categories = CategorySerializer(categories, many=True)
         return Response(serialized_categories.data)
-
-# def get_category_object(id):
-#     return Category.objects.filter(id=id).first()
-#
-# @swagger_auto_schema(
-#     method='GET',
-#     operation_summary="Получить список категорий",
-#     responses={
-#         200: CategorySerializer(many=True),
-#         500: "Серверная ошибка"},
-# )
-# @api_view(['GET'])
-# def get_categories(request):
-#     categories = Category.objects.all()
-#     serializer = CategorySerializer(categories, many=True)
-#     return Response(serializer.data)
-#
-# @swagger_auto_schema(
-#     method='GET',
-#     operation_summary="Получить одну категорию",
-#     responses={
-#         200: CategorySerializer(),
-#         500: "Серверная ошибка"},
-# )
-# @api_view(['GET'])
-# def get_category(request, id):
-#     category = get_category_object(id)
-#     serializer = CategorySerializer(category)
-#     return Response(serializer.data)
-#
-# @swagger_auto_schema(
-#     method='GET',
-#     operation_summary="Получить товары принадлежащие категории",
-#     responses={
-#         200: ProductsSerializer(many=True),
-#         500: "Серверная ошибка"},
-# )
-# @api_view(['GET'])
-# def get_category_products(request, id):
-#     products_in_category = get_category_object(id).related_products
-#     serializer = ProductsSerializer(products_in_category, many=True)
-#     return Response(serializer.data)
- #products = Category.objects.filter(related_products__in=[1, 8]).dispose() #Тестовый запрос на получение категорий выбранных товаров
