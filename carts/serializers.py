4796e25024b748c0b756c28bde4a3a6004d988f7
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 03:33:34 +0300

    cart serializer refactored

--- carts/serializers.py
+++ carts/serializers.py
@@ -6,10 +6,5 @@ from carts.models import Cart
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
-        fields = ['id', 'product', 'user', 'quantity', 'price', 'total']
+        fields = ['id', 'offer', 'user', 'quantity', 'price', 'old_total', 'total']
 
-
-class PostCartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = ['product', 'quantity']
