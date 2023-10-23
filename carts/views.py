37c5f776e8fc088b395e8b8734795f1a56c8b00c
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 03:33:05 +0300

    cart views updated

--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,14 @@
+from django.views.decorators.csrf import csrf_exempt
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 from carts.models import Cart
-from carts.serializers import CartSerializer, PostCartSerializer
-from products.models import Product
+from carts.serializers import CartSerializer
+from offers.models import Offer
 
 
 class CartView(APIView):
@@ -20,7 +22,7 @@ class CartView(APIView):
             500: "Ошибка сервера"},
     )
     def get(self, request):
-        cart = Cart.objects.filter(user_id=request.user.id).all()
+        cart = Cart.objects.filter(user_id=request.user.id)
         if not cart:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CartSerializer(cart, many=True)
@@ -28,29 +30,32 @@ class CartView(APIView):
 
     @swagger_auto_schema(
         operation_summary="Добавить товар в корзину",
-        request_body=PostCartSerializer,
+        request_body=CartSerializer,
         responses={
-            201: PostCartSerializer,
+            201: CartSerializer,
             400: "Неверные данные",
             404: "Не найдено",
             500: "Ошибка сервера",
         },
     )
+    @csrf_exempt
     def post(self, request):
-        product = Product.objects.filter(id=request.data.get("product")).first()
-        if not product:
-            return Response(status=status.HTTP_404_NOT_FOUND)
         quantity = request.data.get("quantity")
+        offer_id = request.data.get("offer")
         if quantity <= 0:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        cart = Cart.objects.filter(product=product, user=request.user).first()
+        offer = get_object_or_404(Offer, id=offer_id)  # Проверка на доступность + извлечение цены
+        if not offer.available:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        cart = Cart.objects.filter(offer_id=offer_id, user=request.user).first()
         if not cart:
             cart = Cart(
-                product_id=product.id,
+                offer_id=offer.id,
                 quantity=quantity,
                 user=request.user,
-                price=product.default_price)
-            cart.total = cart.total_sum()
+                price=offer.price,
+                total=offer.price * quantity
+            )
             cart.save()
             return Response(status=status.HTTP_201_CREATED)
         else:
@@ -60,12 +65,9 @@ class CartView(APIView):
 class SingleCartUtils(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get_cart_object(self, cart_id, user_id):
-        return Cart.objects.filter(id=cart_id, user_id=user_id).first()
-
     @swagger_auto_schema(
         operation_summary="Изменение единицы товара в корзине",
-        request_body=PostCartSerializer,
+        request_body=CartSerializer,
         responses={
             201: CartSerializer,
             204: "Данные удалены",
@@ -75,16 +77,14 @@ class SingleCartUtils(APIView):
         },
     )
     def put(self, request, cart_id):
-        cart = self.get_cart_object(cart_id, request.user.id)
-        if not cart:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if cart.quantity <= 0:
+        quantity = request.data.get("quantity")
+        cart = get_object_or_404(Cart, id=cart_id, user_id=request.user.id)
+        if quantity <= 0:
             cart.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        serializer = CartSerializer(cart, data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
+        cart.quantity = quantity
+        cart.total = cart.price * quantity
+        cart.save()
         return Response(status=status.HTTP_202_ACCEPTED)
 
     @swagger_auto_schema(
@@ -97,9 +97,7 @@ class SingleCartUtils(APIView):
         },
     )
     def delete(self, request, cart_id):
-        cart = self.get_cart_object(cart_id, request.user.id)
-        if not cart:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        cart = get_object_or_404(Cart, id=cart_id, user_id=request.user.id)
         cart.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -112,9 +110,6 @@ class SingleCartUtils(APIView):
         },
     )
     def get(self, request, cart_id):
-        cart = self.get_cart_object(cart_id, request.user.id)
-        if not cart:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+        cart = get_object_or_404(Cart, id=cart_id, user_id=request.user.id)
         serializer = CartSerializer(cart)
         return Response(serializer.data)
-
