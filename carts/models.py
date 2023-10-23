105bc2f17b4e71d60092e57be3c13af62d6a8a4f
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 03:33:12 +0300

    cart models updated

--- carts/models.py
+++ carts/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 
-from products.models import Product
+from offers.models import Offer
 from users.models import User
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, related_name='u_carts', on_delete=models.CASCADE, blank=True)
-    product = models.ForeignKey(Product, related_name='p_carts', blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    offer = models.ForeignKey(Offer, blank=True, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
-    price = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    old_total = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True, blank=True)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-
-    def total_sum(self):
-        return self.price * self.quantity
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name_plural = 'carts'
         ordering = ('-created',)
 
+
 class Promocode(models.Model):
     promocode = models.CharField(max_length=255)
-    discount = models.IntegerField()
+    discount = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'promocodes'
