bd95299781231231977371002cf06005f8d620b3
Vlad <Vlad.Dmitriev.vsu@yandex.ru>
2023-10-23 18:07:23 +0300

    urls added

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('products/', views.ProductsView.as_view()),
     path('products/<int:product_id>/', views.SingleProductView.as_view()),
     path('products/recommends/', views.RecommendedProductsView.as_view()),
+    path('products/edit/<int:product_id>/', views.EditSingleProductView.as_view()),
+    path('products/add/', views.AddSingleProductView.as_view()),
+
 ]
